234ec838124af17a2930542e10343d6f9dc80a05
Justin Mancherje <mancherje.justin@gmail.com>
2016-03-04 16:44:17 -0800

    remove extra comments in server.js

--- server/server.js
+++ server/server.js
@@ -8,22 +8,6 @@ var fs = require('fs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
-// var studentData = fs.readFileSync(__dirname + '/example.json');
-// console.log(studentData);
-// studentData = JSON.parse(studentData);
-// var newArr = [];
-// for (var i=0; i<studentData.length; i++) {
-//   studentData[i].id = studentData[i].id - 1;
-//   newArr.push(studentData[i]);
-// }
-// console.log(newArr);
-// fs.writeFileSync(__dirname + '/students.json', newArr);
-
-
-// DB schemas:
-var userSchema = require('./models/users.js');
-var commentsSchema = require('./models/comments.js');
-
 // connect to mongo in shell:
 // mongo ds039185.mongolab.com:39185/jmancherje_flights -u jmancherje -p justin
 mongoose.connect('mongodb://jmancherje:justin@ds039185.mongolab.com:39185/jmancherje_flights');
